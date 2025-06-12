81d4a4059fd8995150d69d4f4c5d37397435b619
stephenguduri <guduristephen23@gmail.com>
2025-06-12 19:15:22 +0530

    day2_variables.py

--- day2_variables.py
+++ day2_variables.py
@@ -1,1 +1,12 @@
+name= "stephen"
+age= 20
+height= 5.8
+is_student="student"
+
+
+print(f"Hi,I am {name}.I am {age} years old, iam {height} feet tall,and iam a {is_student}. ")
+
+
+
+
 "Day 2: Learned variables, data types, and f-string formatting"
