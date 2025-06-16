88a5d61b59e8fbee9840b08275a5ae65a8c6cb6b
stephenguduri <guduristephen23@gmail.com>
2025-06-16 22:56:46 +0530

    Create age_checker.py

---
+++ age_checker.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age: "))
+
+if age < 13:
+    print("You're a child.")
+elif age >= 13 and age <= 19:
+    print("You're a teenager.")
+else:
+    print("You're an adult.")
